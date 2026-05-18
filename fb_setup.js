ee69ec99cc79e354940b2aeec71b26c11673bba6
23501ap <23501ap@hvhs.school.nz>
2026-05-18 01:30:54 +0000

    wdad

---
+++ fb_setup.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACidkv1AbmKwRtVKdlIomWYHgjK6BNO1s",
+  authDomain: "sal-s-strawberry-saloon-43a67.firebaseapp.com",
+  databaseURL: "https://sal-s-strawberry-saloon-43a67-default-rtdb.firebaseio.com",
+  projectId: "sal-s-strawberry-saloon-43a67",
+  storageBucket: "sal-s-strawberry-saloon-43a67.firebasestorage.app",
+  messagingSenderId: "567717303435",
+  appId: "1:567717303435:web:aad6fc4bc97ac581dfb08e",
+  measurementId: "G-VRXY0GZZLM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- script.js
+++ script.js
@@ -5,3 +5,34 @@ function writeForm(){
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
 }
+
+var GLOBAL_user; // Google's user object
+
+// Set up a listener for the login state of the user.
+function fb_login() {
+authenticationListener = firebase. auth().onAuthStateChanged(fb_handleLogin);
+
+}
+
+// Run when the login state of the user changes.
+function fb_handleLogin(_user) {
+if (_user) {
+console.log("User is logged in")
+GLOBAL_user = _user; // Save the user object to a global variable
+} else {
+console. log("User is NOT logged in - Starting the popup process")
+fb_popupLogin();
+}
+}
+
+
+
+// Run the Google login popup
+function fb_popupLogin() {
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then((result) => {
+GLOBAL_user = result.user; // Save the user object to a global variable
+console.log("User has logged in")
+});
+}
